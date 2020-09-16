694e3349d6888f3b46237ca6ff83c119deb5a8ec
BrenoXavier48 <breno.xavier@upflow.me>
2020-09-15 21:42:06 -0300

    feat: add AppLoading in app

--- mobileApp/App.tsx
+++ mobileApp/App.tsx
@@ -1,22 +1,27 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
+import { AppLoading } from 'expo'
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
 import Home from './src/Home'
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  }) 
+
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
-      <Home/>
+      {
+        fontsLoaded 
+        ? <Home/>
+        : <AppLoading/>
+      }
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
